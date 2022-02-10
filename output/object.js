0c0c908e481a379a9e9531a8fc358e29dba05875
robertogriel <roberto@griel.com.br>
2022-02-10 20:14:36 -0300

    Tipo Objetos {}

---
+++ output/object.js
@@ -0,0 +1,8 @@
+"use strict";
+let serverConfig = {
+    servidor: "Azure",
+    ip: "127.0.0.1",
+    senha: "654321"
+};
+serverConfig = new Array();
+//# sourceMappingURL=object.js.map
---
+++ script/object.ts
@@ -0,0 +1,9 @@
+let serverConfig: object = {
+    servidor: "Azure",
+    ip: "127.0.0.1",
+    senha: "654321"
+}
+
+//serverConfig = "Google"
+
+serverConfig = new Array()
