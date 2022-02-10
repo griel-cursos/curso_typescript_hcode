2052d7699111670804930968d9729b62e8168ad3
robertogriel <roberto@griel.com.br>
2022-02-10 19:55:51 -0300

    União de vários tipos na mesma variável

---
+++ output/union_types.js
@@ -0,0 +1,6 @@
+"use strict";
+let chavePix;
+chavePix = 'teste@pix.com.br';
+chavePix = 165456456;
+console.log(chavePix);
+//# sourceMappingURL=union_types.js.map
---
+++ script/union_types.ts
@@ -0,0 +1,6 @@
+let chavePix: string | number;
+
+chavePix = 'teste@pix.com.br';
+chavePix = 165456456;
+
+console.log(chavePix)
