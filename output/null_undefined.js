ff38d4b9452c4b823ca3f73ca716bf1f61585b15
robertogriel <roberto@griel.com.br>
2022-02-10 20:23:30 -0300

    Tipo null e undefined. Null é ausência de valor. Undefined é quando não tem valor definido

---
+++ output/null_undefined.js
@@ -0,0 +1,6 @@
+"use strict";
+const elementoHtml = document.querySelector("h2");
+let db = "mysql";
+db = null;
+let variavelUndefined;
+//# sourceMappingURL=null_undefined.js.map
---
+++ script/null_undefined.ts
@@ -0,0 +1,12 @@
+//Null é ausência de valor. Undefined é quando não tem valor definido
+
+const elementoHtml: HTMLHeadElement | null = document.querySelector("h2");
+
+let db: string | null = "mysql";
+
+db = null;
+
+//
+
+let variavelUndefined: string | undefined;
+
