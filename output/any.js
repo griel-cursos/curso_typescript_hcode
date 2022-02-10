af1ac0e94c813f14cc3c4ba09b79a9e5abe7a6ed
robertogriel <roberto@griel.com.br>
2022-02-10 20:31:59 -0300

    Tipo any

---
+++ output/any.js
@@ -0,0 +1,5 @@
+"use strict";
+let valor;
+valor = 2015;
+console.log(typeof valor);
+//# sourceMappingURL=any.js.map
---
+++ script/any.ts
@@ -0,0 +1,6 @@
+let valor: any;
+
+valor = 2015;
+//valor = "1020"
+
+console.log(typeof valor)
