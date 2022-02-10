a786597c52ffb8e951069c46aaddbb4d2a5b6579
robertogriel <roberto@griel.com.br>
2022-02-10 19:40:36 -0300

    Dedução de Tipos

---
+++ output/type_interference.js
@@ -0,0 +1,9 @@
+"use strict";
+let linguagem = "Javascript";
+console.log(linguagem);
+console.log(linguagem.toUpperCase());
+const usuarioInfo = [1, "Roberto", new Date()];
+usuarioInfo.push(2);
+usuarioInfo.push("Nay");
+usuarioInfo.push(new Date(2021, 1, 1));
+//# sourceMappingURL=type_interference.js.map
---
+++ script/type_interference.ts
@@ -0,0 +1,16 @@
+let linguagem = "Javascript";
+
+console.log(linguagem)
+
+//linguagem = 2020;
+
+//console.log(linguagem.toFixed());
+
+console.log(linguagem.toUpperCase())
+
+const usuarioInfo = [1, "Roberto", new Date()]
+
+usuarioInfo.push(2);
+usuarioInfo.push("Nay");
+usuarioInfo.push(new Date(2021,1,1));
+// usuarioInfo.push(true); Não permite porque não tem o tipo boolean no array
