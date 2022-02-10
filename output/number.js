40b376bca2bbf6c4e09a79fe09e99a07988b61ec
robertogriel <roberto@griel.com.br>
2022-02-10 19:53:16 -0300

    Números, conversão para números e o bigint

---
+++ output/number.js
@@ -0,0 +1,14 @@
+"use strict";
+let ano;
+let ano2;
+let ano3;
+let ano4;
+ano = 2021;
+ano2 = Number("2020");
+ano3 = +"1994";
+ano4 = parseInt("2015");
+console.log(ano + ano2 + ano3 + ano4);
+console.log("===============================");
+const numeroGrande = 50n;
+console.log(numeroGrande);
+//# sourceMappingURL=number.js.map
---
+++ script/number.ts
@@ -0,0 +1,18 @@
+let ano:number;
+let ano2:number;
+let ano3:number;
+let ano4:number;
+
+ano = 2021;
+ano2 = Number("2020");
+ano3 = +"1994";
+ano4 = parseInt("2015");
+
+
+console.log(ano + ano2 + ano3 + ano4)
+
+console.log("===============================")
+
+const numeroGrande: bigint = 50n;
+
+console.log(numeroGrande)
