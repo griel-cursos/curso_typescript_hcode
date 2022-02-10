0c539ea1b4ef29839dfe0ca1097cfcd5f9c5c7a0
robertogriel <roberto@griel.com.br>
2022-02-10 20:10:52 -0300

    Tipo Tuple (Tipos em ordem e quantidade)

---
+++ script/tuple.ts
@@ -0,0 +1,9 @@
+//Tuple: Define o tipo de dado em ordem e os valores precisam seguir essa ordem
+
+let superset: [string, number, string];
+
+superset = ["TypeScript", 2011, "Anders"];
+
+let diasDaSemana: [string, string, string, string, string, string, string];
+
+diasDaSemana = ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sab"];
