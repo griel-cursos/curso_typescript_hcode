9231873687843c331308d2a4b169607b9c5a5109
robertogriel <roberto@griel.com.br>
2022-02-10 20:00:37 -0300

    Tipo Boollean (True e False)

---
+++ output/boolean.js
@@ -0,0 +1,11 @@
+"use strict";
+let admin = true;
+admin = true;
+admin = Boolean('');
+console.log(admin);
+console.log("=============");
+let senha = "1234";
+let senhaSegura;
+senhaSegura = senha.length > 8;
+console.log(senhaSegura);
+//# sourceMappingURL=boolean.js.map
---
+++ script/boolean.ts
@@ -0,0 +1,16 @@
+let admin: boolean = true;
+
+admin = true;
+
+admin = Boolean(''); //False
+
+console.log(admin)
+
+console.log("=============")
+
+let senha: string = "1234";
+let senhaSegura: boolean;
+
+senhaSegura = senha.length > 8;
+
+console.log(senhaSegura)
