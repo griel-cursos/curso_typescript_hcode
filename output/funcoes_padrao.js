93fab3086d72c1e006e2f779281b0d5e50b85719
robertogriel <roberto@griel.com.br>
2022-02-12 10:27:40 -0300

    Funções com parâmetros padrão e opcionais

---
+++ output/funcoes_padrao.js
@@ -0,0 +1,8 @@
+"use strict";
+const enviarEmail = (para, assunto = "Sem assunto", remetente) => {
+    console.log({
+        para, assunto, remetente
+    });
+};
+enviarEmail('roberto@griel.com', 'Typescript');
+//# sourceMappingURL=funcoes_padrao.js.map
