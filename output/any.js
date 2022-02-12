652c95a785ab4cfcafe0b76bf7d140ee3b1d6edf
robertogriel <roberto@griel.com.br>
2022-02-12 09:49:05 -0300

    Tipo Any

--- output/any.js
+++ output/any.js
@@ -2,4 +2,9 @@
 let valor;
 valor = 2015;
 console.log(typeof valor);
+let despesaFixa;
+let despesaVariavel;
+despesaFixa = "150";
+despesaVariavel = 320;
+console.log(despesaFixa + despesaVariavel);
 //# sourceMappingURL=any.js.map
--- script/any.ts
+++ script/any.ts
@@ -4,3 +4,12 @@ valor = 2015;
 //valor = "1020"
 
 console.log(typeof valor)
+
+let despesaFixa: any;
+let despesaVariavel: number;
+
+despesaFixa = "150";
+despesaVariavel = 320;
+
+console.log(despesaFixa + despesaVariavel) //Irá retornar um erro porque um valor é string
+
