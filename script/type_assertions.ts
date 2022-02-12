434cac8a16b1e90f3fe706febdf69ed8e4013a28
robertogriel <roberto@griel.com.br>
2022-02-12 10:09:27 -0300

    Afirmação de tipos

--- script/type_assertions.ts
+++ script/type_assertions.ts
@@ -5,7 +5,7 @@ const site: unknown = 'https://www.griel.dev.br';
 let siteFavoritos: Array<string> = [];
 
 siteFavoritos.push(site as string);
-
-siteFavoritos.push(<string>site);
+siteFavoritos.push(<string>site); //Não funcionará no React
+//Estou afirmando o tipo da variavel
 
 console.log('=====================')
